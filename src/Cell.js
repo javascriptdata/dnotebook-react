a5853ac51238a1914250eeee601fb15826dc1535
Helegbedey <devaze007@gmail.com>
2020-12-11 21:24:37 +0100

    structuring project folder and added code formatting

--- src/Cell.js
+++ src/Cell.js
@@ -1,125 +1,171 @@
-import React, {useRef, useEffect} from 'react'
+import React, { useRef, useEffect } from "react";
 import CodeMirror from "react-codemirror";
-import { Remarkable } from 'remarkable';
+import { Remarkable } from "remarkable";
 
 require("codemirror/lib/codemirror.css");
 require("codemirror/mode/javascript/javascript");
 require("codemirror/theme/dracula.css");
 
-export default function Cell({cell, dispatch, currentCell, setCurrentCell,cellId}) {
+export default function Cell({
+  cell,
+  dispatch,
+  currentCell,
+  setCurrentCell,
+  cellId,
+}) {
   const refCode = useRef(null);
-  const refOutput = useRef('');
+  const refOutput = useRef("");
 
-  const getCode = ()=>{
-    if(cell.type === "code"){
+  const getCode = () => {
+    if (cell.type === "code") {
       const input = refCode.current.getCodeMirror().getValue();
       try {
-       const output  = ("global",eval)(input) || ''
-       const cellstate = {...cell, input: input, output:output}
-       dispatch({type:"CHANGE_CELL", payload:cellstate});
-      }catch(error){
-        const cellstate = {...cell, input: input, output:error}
-       dispatch({type:"CHANGE_CELL", payload:cellstate});
+        // eslint-disable-next-line no-eval
+        const output = ("global", eval)(input) || "";
+        const cellstate = { ...cell, input, output };
+        dispatch({ type: "CHANGE_CELL", payload: cellstate });
+      } catch (error) {
+        const cellstate = { ...cell, input, output: error };
+        dispatch({ type: "CHANGE_CELL", payload: cellstate });
       }
-       
-
-    }else{
-      const cellstate = {...cell, input: refCode.current.value}
+    } else {
+      const cellstate = { ...cell, input: refCode.current.value };
       showOutput();
-      dispatch({type:"CHANGE_CELL", payload:cellstate});
+      dispatch({ type: "CHANGE_CELL", payload: cellstate });
     }
-  }
+  };
 
-  const setId = ()=>{
-    const id = currentCell? currentCell : parseInt(cell.id.split("_")[1]);
+  const setId = () => {
+    const id = currentCell || parseInt(cell.id.split("_")[1]);
     setCurrentCell(id);
-  }
+  };
 
-  useEffect(()=>{
-    if(cell.type ==="text"){
+  useEffect(() => {
+    if (cell.type === "text") {
       refCode.current.value = cell.input;
       const md = new Remarkable();
       refOutput.current.innerHTML = md.render(cell.input);
-      refCode.current.style.display = cell.input ? "none": "block";
-    }else
-    {
+      refCode.current.style.display = cell.input ? "none" : "block";
+    } else {
       refCode.current.getCodeMirror().setValue(cell.input);
       refOutput.current.innerHTML = cell.output;
     }
     setId();
-  },[cell])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cell]);
 
-  const upCell = (type)=>{
+  const upCell = (type) => {
+    const id = cellId - 1;
+    newCell(id, type);
+  };
 
-    const id = cellId -1;
-    newCell(id,type);
-  }
-
-  const downCell = (type)=>{
+  const downCell = (type) => {
     const id = cellId;
-    newCell(id,type);
-    
-  }
+    newCell(id, type);
+  };
 
-  const newCell = (id, type)=>{
-
+  const newCell = (id, type) => {
     const newCell = {
-      id: "cell_"+(currentCell+1),
+      id: `cell_${currentCell + 1}`,
       input: "",
-    }
+    };
 
-    if(type === "text"){
-      newCell["type"] = "text"
-    }else{
-      newCell["output"] = ""
-      newCell["type"] = "code"
+    if (type === "text") {
+      newCell.type = "text";
+    } else {
+      newCell.output = "";
+      newCell.type = "code";
     }
-    setCurrentCell(currentCell+1);
-    dispatch({type:"ADD_CELL", payload:newCell, currentId: id});
-  }
+    setCurrentCell(currentCell + 1);
+    dispatch({ type: "ADD_CELL", payload: newCell, currentId: id });
+  };
 
-  const disableOutput = ()=>{
-    if(cell.type === "text"){
+  const disableOutput = () => {
+    if (cell.type === "text") {
       refOutput.current.style.display = "none";
       refCode.current.style.display = "block";
     }
-  }
+  };
 
-  const showOutput = ()=>{
-    if(cell.type === "text"){
+  const showOutput = () => {
+    if (cell.type === "text") {
       refOutput.current.style.display = "block";
       refCode.current.style.display = "none";
     }
-  }
+  };
 
-  const deleteCell = ()=>{
-    dispatch({type:"DELETE_CELL",payload:cell.id});
-  }
+  const deleteCell = () => {
+    dispatch({ type: "DELETE_CELL", payload: cell.id });
+  };
 
   return (
     <>
-    <button onClick={()=>{getCode()}}>Run</button>
-    <button onClick={()=>{upCell("code")}}>Up cell</button>
-    <button onClick={()=>{downCell("code")}}>down cell</button>
-    <button onClick={()=>{upCell("text")}}>Text up</button>
-    <button onClick={()=>{downCell("text")}}>Text down</button>
-    <button onClick={()=>{deleteCell()}}>Delete Cell</button>
-      {cell.type === "code" ? <CodeMirror value={cell.input} ref={refCode}
-        options={{
-          tabSize: 2,
-          theme: "dracula",
-          lineNumbers: true,
-          mode: "javascript"
+      <button
+        onClick={() => {
+          getCode();
         }}
-      /> : <TextCell refText={refCode}/>}
-      <div ref={refOutput} onClick={()=>{disableOutput()}}></div>
-  </>
-  )
+      >
+        Run
+      </button>
+      <button
+        onClick={() => {
+          upCell("code");
+        }}
+      >
+        Up cell
+      </button>
+      <button
+        onClick={() => {
+          downCell("code");
+        }}
+      >
+        down cell
+      </button>
+      <button
+        onClick={() => {
+          upCell("text");
+        }}
+      >
+        Text up
+      </button>
+      <button
+        onClick={() => {
+          downCell("text");
+        }}
+      >
+        Text down
+      </button>
+      <button
+        onClick={() => {
+          deleteCell();
+        }}
+      >
+        Delete Cell
+      </button>
+      {cell.type === "code" ? (
+        <CodeMirror
+          value={cell.input}
+          ref={refCode}
+          options={{
+            tabSize: 2,
+            theme: "dracula",
+            lineNumbers: true,
+            mode: "javascript",
+          }}
+        />
+      ) : (
+        <TextCell refText={refCode} />
+      )}
+      <div
+        ref={refOutput}
+        onClick={() => {
+          disableOutput();
+        }}
+      ></div>
+    </>
+  );
 }
 
-function TextCell({refText}){
-
-  return (
-  <textarea ref={refText}></textarea>
-  )
+function TextCell({ refText }) {
+  return <textarea ref={refText}></textarea>;
 }
--- src/utils.js
+++ src/utils.js
@@ -1,107 +1,102 @@
 export function print_val(val) {
-        /// Neeed refactoring. Lot of duplicated line of code
-        if (Array.isArray(val[0])) {
-            let row_length = val.length;
-            let data_string = "[";
-            if (row_length > 10) {
-
-                for (let i = 0; i < 10; i++) {
-                    let row_val = val[i]
-                    let col_length = row_val.length;
-                    data_string += "[";
-                    if (col_length > 10) {
-                        for (let j = 0; j < 10; j++) {
-                            data_string += j === 9 ? `${row_val[j]}` : `${row_val[j]},`
-                        }
-                        data_string += `.......${col_length - 10} more],`
-                    } else {
-
-                        for (let j = 0; j < col_length; j++) {
-                            data_string += j === (col_length - 1) ? `${row_val[j]}` : `${row_val[j]},`
-                        }
-                        data_string += i === (row_length - 1) ? "]" : "],"
-                    }
-                }
-                data_string += `...${row_length - 10} more]`
-            }
-            else {
-                for (let i = 0; i < row_length; i++) {
-                    let row_val = val[i]
-                    let col_length = row_val.length;
-                    data_string += "[";
-
-                    if (col_length > 10) {
-                        for (let j = 0; j < 10; j++) {
-                            data_string += j === 9 ? `${row_val[j]}` : `${row_val[j]},`
-                        }
-                        data_string += `.......${col_length - 10} more],`
-
-                    } else {
-                        for (let j = 0; j < col_length; j++) {
-                            data_string += j === (col_length - 1) ? `${row_val[j]}` : `${row_val[j]},`
-                        }
-                        data_string += i === (row_length - 1) ? "]" : "],"
-                    }
-                }
-                data_string += "]"
-            }
-            return data_string
-
+  /// Neeed refactoring. Lot of duplicated line of code
+  if (Array.isArray(val[0])) {
+    const row_length = val.length;
+    let data_string = "[";
+    if (row_length > 10) {
+      for (let i = 0; i < 10; i++) {
+        const row_val = val[i];
+        const col_length = row_val.length;
+        data_string += "[";
+        if (col_length > 10) {
+          for (let j = 0; j < 10; j++) {
+            data_string += j === 9 ? `${row_val[j]}` : `${row_val[j]},`;
+          }
+          data_string += `.......${col_length - 10} more],`;
         } else {
-
-            let row_length = val.length;
-            let data_string = "["
-            let count = row_length > 10 ? 10 : row_length
-
-            for (let i = 0; i < count; i++) {
-                data_string += i === (count - 1) ? `${val[i]}` : `${val[i]},`
-            }
-
-            let diff = row_length - count;
-
-            if (diff > 0) {
-                data_string += `....${diff} more]`
-            } else {
-                data_string += "]";
-            }
-            return data_string;
-
+          for (let j = 0; j < col_length; j++) {
+            data_string +=
+              j === col_length - 1 ? `${row_val[j]}` : `${row_val[j]},`;
+          }
+          data_string += i === row_length - 1 ? "]" : "],";
         }
-    }
+      }
+      data_string += `...${row_length - 10} more]`;
+    } else {
+      for (let i = 0; i < row_length; i++) {
+        const row_val = val[i];
+        const col_length = row_val.length;
+        data_string += "[";
 
-  /**
+        if (col_length > 10) {
+          for (let j = 0; j < 10; j++) {
+            data_string += j === 9 ? `${row_val[j]}` : `${row_val[j]},`;
+          }
+          data_string += `.......${col_length - 10} more],`;
+        } else {
+          for (let j = 0; j < col_length; j++) {
+            data_string +=
+              j === col_length - 1 ? `${row_val[j]}` : `${row_val[j]},`;
+          }
+          data_string += i === row_length - 1 ? "]" : "],";
+        }
+      }
+      data_string += "]";
+    }
+    return data_string;
+  }
+
+  const row_length = val.length;
+  let data_string = "[";
+  const count = row_length > 10 ? 10 : row_length;
+
+  for (let i = 0; i < count; i++) {
+    data_string += i === count - 1 ? `${val[i]}` : `${val[i]},`;
+  }
+
+  const diff = row_length - count;
+
+  if (diff > 0) {
+    data_string += `....${diff} more]`;
+  } else {
+    data_string += "]";
+  }
+  return data_string;
+}
+
+/**
  * Displays Danfo DataFrame/Series in a formated table
- * @param {DataFrame} df 
+ * @param {DataFrame} df
  */
 function table(df) {
-    const { col_types, series, columns, index, values } = df;
-    let head = ""
+  const { col_types, series, columns, index, values } = df;
+  let head = "";
+
+  if (series) {
+    head += `<th class="${col_types[0]}">${columns}</th>`;
+  } else {
+    columns.forEach((name, i) => {
+      head += `<th class="${col_types[i]}">${name}</th>`;
+    });
+  }
+
+  let body = "";
+  values.forEach((row, i) => {
+    let b_String = `<tr><th>${index[i]}</th>`;
 
     if (series) {
-        head += `<th class="${col_types[0]}">${columns}</th>`
+      b_String += `<td class="${col_types[0]}">${row}</td>`;
     } else {
-        columns.forEach((name, i) => {
-            head += `<th class="${col_types[i]}">${name}</th>`
-        });
+      row.forEach((v, j) => {
+        b_String += `<td class="${col_types[j]}">${v}</td>`;
+      });
     }
 
-    let body = ""
-    values.forEach((row, i) => {
-        let b_String = `<tr><th>${index[i]}</th>`
+    b_String += "</tr>";
+    body += b_String;
+  });
 
-        if (series) {
-            b_String += `<td class="${col_types[0]}">${row}</td>`
-        } else {
-            row.forEach((v, j) => {
-                b_String += `<td class="${col_types[j]}">${v}</td>`
-            });
-        }
-
-        b_String += "</tr>"
-        body += b_String;
-    });
-
-    const table = `
+  const table = `
         <div style="overflow: auto; max-height: 300px;"><table class="df-table" border="1">
         <thead>
             <tr>
@@ -114,41 +109,41 @@ function table(df) {
         </tbody>
         </table>
         </div>
-    `
-    return table;
+    `;
+  return table;
 }
 
-export const downLoad_notebook = (state)=>{
-    const data = JSON.stringify(state.cells);
-    const blob = new Blob([data], { "type": "application/json" })
-    const url = (window.URL || window.webkitURL).createObjectURL(blob);
+export const downLoad_notebook = (state) => {
+  const data = JSON.stringify(state.cells);
+  const blob = new Blob([data], { type: "application/json" });
+  const url = (window.URL || window.webkitURL).createObjectURL(blob);
 
-    const link = document.createElement('a');
-    const name = "Dnote-react"
-    link.download = `${name}.json`;
-    link.href = url;
+  const link = document.createElement("a");
+  const name = "Dnote-react";
+  link.download = `${name}.json`;
+  link.href = url;
 
-    document.body.appendChild(link);
-    link.click()
-    link.remove()
+  document.body.appendChild(link);
+  link.click();
+  link.remove();
+};
+
+export const load_notebook = (dispatch) => {
+  const { files } = document.getElementById("import-notebook-file");
+  let json_content = null;
+  if (files.length > 0) {
+    const content = files[0];
+    const reader = new FileReader();
+    reader.onload = function (t) {
+      json_content = t.target.result;
+      const json = JSON.parse(json_content);
+      dispatch({ type: "LOAD_NOTE", payload: json });
+    };
+    reader.readAsText(content);
   }
+};
 
-export const load_notebook = (dispatch)=>{
-    const files = document.getElementById("import-notebook-file").files;
-    let json_content = null;
-    if (files.length > 0){
-      const content = files[0];
-      const reader = new FileReader();
-      reader.onload = function(t){
-        json_content = t.target.result;
-        const json = JSON.parse(json_content);
-        dispatch({type:"LOAD_NOTE", payload:json});
-      }
-      reader.readAsText(content);
-    }
-  }
-
-export const makeGlobal = ()=>{
-  window["print_val"] = print_val;
-  window['table'] = table;
-}
+export const makeGlobal = () => {
+  window.print_val = print_val;
+  window.table = table;
+};
